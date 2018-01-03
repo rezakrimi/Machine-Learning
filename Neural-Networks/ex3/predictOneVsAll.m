c7a69e571af6c4c0de3b54ae025e43f07fc280c3
Reza Karimi <reza@Rezas-MacBook-Pro.local>
2018-01-03 00:50:42 -0500

    predictOnveVsAll.m predicts the accuracy of the trained model

--- Neural-Networks/ex3/predictOneVsAll.m
+++ Neural-Networks/ex3/predictOneVsAll.m
@@ -29,8 +29,11 @@ X = [ones(m, 1) X];
 %       are in rows, then, you can use max(A, [], 2) to obtain the max 
 %       for each row.
 %       
+results = X * all_theta';
 
+[predicts, indexes] = max(results,[],2);
 
+p = indexes;
 
 
 
