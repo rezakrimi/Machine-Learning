e5df530e51c038f275f43492960d4f6ef0a3861c
Reza Karimi <reza@Rezas-MacBook-Pro.local>
2018-04-04 01:05:44 -0400

    completing k-nearest neighbours

--- Sentdex-Tutorials/test.py
+++ Sentdex-Tutorials/test.py
@@ -1,11 +1,14 @@
 import pandas as pd
 import numpy as np
 
-df = pd.DataFrame(np.arange(12).reshape(3,4),
-                      columns=['A', 'B', 'C', 'D'])
-
-print (df.drop([1], 0))
-
-a= [1, 2, 3, 4, 5]
-
-print (a[-3:])
+# df = pd.DataFrame(np.arange(12).reshape(3,4),
+#                       columns=['A', 'B', 'C', 'D'])
+#
+# print (df.drop([1], 0))
+#
+# a= [1, 2, 3, 4, 5]
+#
+# print (a[-3:])
+a = np.array([[1,2,3],[1,1,1]])
+b = np.array([[1,2],[1,2],[1,2]])
+print(np.dot(a, b))
